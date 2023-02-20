df9e8f678a304fa04f1e1b78a3eaddc71b0bacc7
shu-william <williamshu@outlook.com>
2023-02-20 06:19:15 -0800

    added simple validations to form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -24,22 +24,47 @@ const Form = (props) => {
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">First Name:</label>
                     <input type="text" name="firstName" id="firstName" className='form-control' onChange={(e) => {setFirstName(e.target.value)}}/>
+                    {
+                        firstName.length < 2 && firstName.length > 0 ?
+                        <p className='error'>First Name must be at least 2 characters.</p> :
+                        <p></p>
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Last Name:</label>
                     <input type="text" name="lastName" id="lastName" className='form-control' onChange={(e) => {setLastName(e.target.value)}}/>
+                    {
+                        lastName.length < 2 && lastName.length > 0 ?
+                        <p className='error'>Last Name must be at least 2 characters.</p> :
+                        <p></p>
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Email:</label>
                     <input type="email" name="email" id="email" className='form-control' onChange={(e) => {setEmail(e.target.value)}}/>
+                    {
+                        email.length < 5 && email.length > 0 ?
+                        <p className='error'>Email must be at least 5 characters.</p> :
+                        <p></p>
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Password:</label>
                     <input type="password" name="password" id="password" className='form-control' onChange={(e) => {setPassword(e.target.value)}}/>
+                    {
+                        password.length < 8 && password.length > 0 ?
+                        <p className='error'>Password must be at least 8 characters.</p> :
+                        <p></p>
+                    }
                 </div>
                 <div className="form-group mt-3">
                     <label htmlFor="" className="form-label">Confirm Password:</label>
                     <input type="password" name="confPassword" id="confPassword" className='form-control' onChange={(e) => {setConfPassword(e.target.value)}}/>
+                    {
+                        password != confPassword && confPassword.length > 0 ?
+                        <p className='error'>Passwords must match.</p> :
+                        <p></p>
+                    }
                 </div>
                 <input type="submit" value="Submit" className='btn btn-primary mt-3' />
             </form>
